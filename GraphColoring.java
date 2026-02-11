2bc451f30685ac80d1a2625abce5bb6eaaa78013
Athenkosi Miya <athenkosiluyanda98@gmail.com>
2026-02-12 00:07:57 +0200

    Added comments

--- GraphColoring.java
+++ GraphColoring.java
@@ -1,5 +1,10 @@
+
 import java.util.ArrayList;
 import java.util.HashSet;
+
+// Implements the Greedy Graph Coloring algorithm.
+// Time Complexity: O(V + E) assuming bounded color usage.
+// The number of colors used depends on vertex ordering.
 
 public class GraphColoring{
 
@@ -8,30 +13,37 @@ public class GraphColoring{
          colorGraph(towersArr);
     }
 
+    // Greedy coloring algorithm:
+    // For each vertex:
+    //      1. Collect colors used by adjacent vertices.
+    //      2. Assign the smallest non-conflicting color.
+    // This does not guarentee optimal coloring (graph coloring is NP-Hard);
+    // but runs efficiently for large sparse graphs.
     public void colorGraph(ArrayList<TowerCell> towersArr){
 
         for(TowerCell current: towersArr){
 
-            //Track colors used by neighbors
+            // Tracks colors currently used by neighbors
+            // HashSet ensures O(1) average lookup
             HashSet<Integer> usedColors = new HashSet<>();
 
+            // Build set of forbidden colors
             for(TowerCell neighbor: current.getNeighbours()){
                 if(neighbor.getColouring() != -1){
                     usedColors.add(neighbor.getColouring());
                 }    
             }
             
-            //assign the smallest available color
+            // Assign the smallest available color
             int color = 0;
             while(usedColors.contains(color)){
                 color++;
             }
 
             current.setColouring(color);
-            
         }
 
-        //Print results
+        // Print results
         for(TowerCell cell : towersArr){
             System.out.println(cell.getTowerID() + "--> Color: " + cell.getColouring());
         }
